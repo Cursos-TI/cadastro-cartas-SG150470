7b6e0b38eb7255253d0dc2cbe708982fed7b9d39
SG150470 <magalhaes.santosms@hotmail.com>
2025-09-16 18:07:12 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  
   char cidade[50] = Rio de Janeiro;
   char codigo [4] = 'A01';
   int População = 12325000;
@@ -13,30 +14,33 @@ int main() {
   float PIB = 699.28;
   int pontoTuristico = 50;
   // Área para entrada de dados
-  printf("Digite o codigo: %c\n");
+  printf("Digite o codigo: ");
   scanf("%c" , &codigo);
-  
-  printf("Digite a cidade é: %s\n" , cidade);
-  scanf("%s" , &cidade);
 
-  printf("Digite a populaçao: %f\n" , Populacao);
+  printf("Digite a populaçao: \n");
   scanf("%f\n" , &Populacao);
 
-  printf("Digite a Area: %f\n" , Area);
-  scanf("%f"\n , Area);
+  printf("Digite a Area: \n");
+  scanf("%f"\n ,&Area);
 
   printf(Digite o PIB: \n");
-  scanf("%f"\n , PIB);
+  scanf("%f"\n , &PIB);
 
   printf("Digite o ponto Turistico: \n");
-  scanf("%d"\n , Turistico);  
-  
-  
+  scanf("%d"\n , &Turistico);  
   
 
-  
 
   // Área para exibição dos dados da cidade
+
+  printf("A cidade é: %s\n" , cidade);
+  printf("O codigo é: %c\n" , codigo);
+  printf("A Populacao é: %f\n" , Populacao);
+  printf("A Area é: %f\n" , Area);
+  printf("O PIB é : %f\n" , PIB);
+  printf("O ponto Turistico é: %d\n", pontoTuristico);
+
+  
 
 return 0;
 } 
